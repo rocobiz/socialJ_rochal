f890c70a6539d0741d5290aad8db99f9fbd57393
rocobiz <32499355+rocobiz@users.noreply.github.com>
2018-06-18 22:14:08 -0500

    Create SocialJApplication.java

---
+++ src/main/java/com/socialj/SocialJApplication.java
@@ -0,0 +1,12 @@
+package com.socialj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialJApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialJApplication.class, args);
+	}
+}
