e27d6d73bd30f323ca7acd6db2054db1377c3d0e
rocobiz <32499355+rocobiz@users.noreply.github.com>
2018-06-18 22:12:23 -0500

    Create socialjInfoRepository.java

---
+++ src/main/java/com/socialj/repository/socialjInfoRepository.java
@@ -0,0 +1,9 @@
+package com.socialj.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.socialj.model.socialjInfo;
+
+public interface socialjInfoRespository extends JpaRepository<socialjInfo, Integer>{
+
+}
